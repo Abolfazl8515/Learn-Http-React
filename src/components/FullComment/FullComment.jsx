8a939f9f204c0b6de1e4806a1fe0ebccdf70aefb
Abolfazl <boorbooryabolfazl85@gmail.com>
2023-04-21 21:12:58 +0330

    added delete comment

--- src/components/FullComment/FullComment.jsx
+++ src/components/FullComment/FullComment.jsx
@@ -11,12 +11,20 @@ const FullComment = ({ commentId }) => {
         .catch();
     }
   }, [commentId]);
+  const deleteHandler = () => {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/comments/${commentId}`)
+      .then((res) => console.log(res.data));
+  };
   if (!commentId) return <div>Select one comment to see details</div>;
   return (
     <section>
       <p>Name is : {fullComment.name}</p>
       <p>Email is : {fullComment.email}</p>
       <p>body is : {fullComment.body}</p>
+      <button type="button" onClick={deleteHandler}>
+        Delete
+      </button>
     </section>
   );
 };
